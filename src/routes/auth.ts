8eef7f5b044c39a77cc8dbe958c11794eea07ba2
Rido Atmanto <ridoatmanto@gmail.com>
2024-07-29 19:55:34 +0700

    feat: add auth

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,62 +1,88 @@
 import { Hono } from "hono";
-import { HTTPException } from "hono/http-exception";
+import { zValidator } from "@hono/zod-validator";
+
+import { z } from "zod";
 import { prisma } from "../libs/prisma";
-import { escapeBigInt } from "../libs/escape-big-int";
+import { createToken } from "../libs/jwt";
+import { checkUserToken } from "../middlewares/check-user-token";
+import { verifyPassword } from "../libs/password";
 
 const app = new Hono();
 
-app.get("/", async (c) => {
-  try {
-    const searchQuery = c.req.query("q");
+app.post(
+  "/login",
+  zValidator(
+    "json",
+    z.object({
+      email: z.string(),
+      password: z.string(),
+    })
+  ),
+  async (c) => {
+    const body = c.req.valid("json");
 
-    if (!searchQuery) {
-      const allProducts = await prisma.product.findMany({
-        where: {},
-        orderBy: [{ createdAt: "desc" }],
-      });
-
-      return c.json(escapeBigInt(allProducts));
-    }
-
-    const searchProduct = await prisma.product.findMany({
-      where: {
-        name: {
-          contains: searchQuery,
-          mode: "insensitive",
-        },
-      },
+    const foundUser = await prisma.user.findUnique({
+      where: { email: body.email },
+      include: { password: { select: { hash: true } } },
     });
 
-    return c.json(escapeBigInt(searchProduct));
-  } catch (err: any) {
-    console.log(err.message);
-    throw new HTTPException(401, { message: err.message });
+    if (!foundUser) {
+      c.status(404);
+      return c.json({ message: "Cannot login because user not found" });
+    }
+
+    if (!foundUser?.password?.hash) {
+      c.status(400);
+      return c.json({
+        message: "Cannot login because user doesn't have a password",
+      });
+    }
+
+    const validPassword = await verifyPassword(
+      foundUser.password.hash,
+      body.password
+    );
+
+    if (!validPassword) {
+      c.status(400);
+      return c.json({
+        message: "Password incorrect",
+      });
+    }
+
+    const token = await createToken(foundUser.id);
+
+    if (!token) {
+      c.status(400);
+      return c.json({ message: "Token failed to create" });
+    }
+
+    return c.json({
+      message: "Login successful",
+      token,
+    });
   }
+);
+
+app.get("/me", checkUserToken(), async (c) => {
+  const user = c.get("user");
+
+  const userData = await prisma.user.findUnique({
+    where: { id: user.id },
+  });
+
+  return c.json({
+    message: "User data",
+    user: userData,
+  });
 });
 
-app.get("/:slug", async (c) => {
-  try {
-    const slugParam = c.req.param("slug");
-
-    if (!slugParam) {
-      c.status(204);
-      return c.json({ message: "Product ID needed" });
-    }
-
-    const product = await prisma.product.findFirst({
-      where: { slug: slugParam },
-    });
-
-    if (product == null) {
-      c.status(204);
-      return c.json({ message: "Product doesn't exists!" });
-    }
-
-    return c.json(escapeBigInt(product));
-  } catch (err: any) {
-    console.log(err.message);
-    throw new HTTPException(401, { message: err.message });
-  }
+app.get("/logout", checkUserToken(), async (c) => {
+  // Note: might be unnecessary since this is token-based auth
+  // We can just remove the token on the client or frontend
+  return c.json({
+    message: "Logout",
+  });
 });
 
 export const auth = app;
