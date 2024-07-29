718bc9ed9b6f2e95c24d30eaf5d44708b77c87f5
Rido Atmanto <ridoatmanto@gmail.com>
2024-07-29 19:56:54 +0700

    Delete src/libs/escapeBigInt.ts

--- src/libs/escapeBigInt.ts
+++
@@ -1,6 +0,0 @@
-export function escapeBigInt(jsonDataWithBigInt: object): string {
-  return JSON.stringify(
-    jsonDataWithBigInt,
-    (key, value) => (typeof value === "bigint" ? value.toString() : value) // return everything else unchanged
-  );
-}
